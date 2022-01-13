685359fcf3d63986d073af484b17be6a22e4b939
马东001 <847364864@qq.com>
2022-01-13 09:02:25 +0800

    systemUiVisibility废弃了换getWindowInsetsController

--- app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
@@ -31,10 +31,13 @@ class WeatherActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val decorView = window.decorView
-        decorView.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        window.statusBarColor = Color.TRANSPARENT
+        // getWindowInsetsController的使用可以参考https://juejin.cn/post/6940048488071856164
+        val controller = ViewCompat.getWindowInsetsController(window.decorView)
+        controller?.isAppearanceLightStatusBars = false
+//        val decorView = window.decorView
+//        decorView.systemUiVisibility =
+//            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//        window.statusBarColor = Color.TRANSPARENT
         setContentView(R.layout.activity_weather)
         if (viewModel.locationLng.isEmpty()) {
             viewModel.locationLng = intent.getStringExtra("location_lng") ?: ""
